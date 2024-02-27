7e2281e69effb4332fd5a8ee534a0d60fca1fdc3
Konrad1991 <konrad_kraemer@yahoo.de>
2024-02-27 14:43:58 +0100

    added script for running installation and testing

--- test_tsf.sh
+++ test_tsf.sh
@@ -3,18 +3,19 @@
 Rscript -e 'roxygen2::roxygenize("tsf")' && R CMD INSTALL tsf
 
 Rscript -e '
+set.seed(123)
 library(tsf)
 library(ggplot2)
+library(patchwork)
 path <- paste0(system.file("examples", package = "tsf"), "/IDA.txt")
 res <- opti("ida", c(1, 0, 0, 0), c(10^9, 10^6, 10^6, 10^6), path, c(4.3, 6.0, 7079458))
-lapply(1:length(res[[3]]), function(x) {
-  plot(res[[3]][[x]])
-  ggsave(paste0("File_", x, ".png"))
-})
+p <- res[[3]][[1]] / res[[3]][[2]]
+plot(p)
+ggsave("Overview.png")
 resSensitivity <- sensitivity("ida", res[[2]], path, c(4.3, 6.0, 7079458))
-lapply(1:length(resSensitivity, function(x) {
-  plot(resSensitivity)
-  ggsave(paste0("File_", x, ".png"))
-})
+p <- (resSensitivity[[1]] + resSensitivity[[2]]) /
+     (resSensitivity[[3]] + resSensitivity[[4]]) /
+     resSensitivity[[5]]
+ggsave("Sensitivity.png")
 '
 
