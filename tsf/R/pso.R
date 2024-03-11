d9df1ea76eb0e72f4aad4821b5c1c952b2a268c2
Konrad1991 <konrad_kraemer@yahoo.de>
2024-03-11 11:12:00 +0100

    added docu for pso

--- tsf/R/pso.R
+++ tsf/R/pso.R
@@ -1,7 +1,71 @@
-# particle swarm optimization
+#' Particle swarm optimization
+#'
+#' @description Interface to the particle swarm optimization (pso) algorithm. 
+#' 
+#' @export
+#' @rawNamespace import(shiny, except=c(dataTableOutput, renderDataTable, runExample))
+#' @rawNamespace import(shinyWidgets, except=c(alert))
+#' @import shinydashboard
+#' @import shinyjs
+#' @import openxlsx
+#' @import future
+#' @import promises
+#' @import DT
+#' @param env is something that is passed to the loss function in addition to the parameters which get optimized
+#' @param lb is a numeric vector defining the lower boundaries of the parameter 
+#' @param ub is a numeric vector defining the upper boundaries of the parameter 
+#' @param loss is the loss function for which the optimal parameter set should be found
+#' @param ngen is the number of generations the pso should run
+#' @param npop is the number of particles which should be used during optimization
+#' @param error_threshold is a number defining a sufficient small error at which the optimization is stopped
+#' @param global is a logical parameter. If set to TRUE a global star topology is used. 
+#'        Thus, each particle compares itself with the global best particle of the entire swarm. In contrast,
+#'        if global is set to FALSE a random arbitrary neighborhood is used instead. Thus, each particle has
+#'        a neighberhood which contains K neighbours where K is between 0 and 3. From the swarm K neighbours are
+#'        drawn randomly. From the neighberhood the best particle is used for comparison. The neighberhood is
+#'        calculated for each generation. 
+#' @param saveSwarm is a logical value defining whether the entire optimization should be saved.
+#' @param runAsShiny is an internal parameter which is used when running the shiny app interface.
+#' @examples
+#' rosenbrock <- function(parameter, env, Ignore) {
+#'  value <- 0
+#'  for (i in 1:(length(parameter) - 1)) {
+#'    value <- value + 
+#'      100*(parameter[i + 1] - parameter[i]^2)^2 +
+#'      (1 - parameter[i])^2
+#'  }
+#' return(value)
+#'}
+#'
+#' set.seed(1234)
+#' res <- tsf::pso(new.env(), rep(-10, 3), rep(10, 3), rosenbrock, 1000, 40,
+#'                  0.00001, TRUE, FALSE)
 pso <- function(env, lb, ub, loss, ngen, npop, error_threshold, global = FALSE, 
-                saveSwarm = FALSE, runAsShiny = NULL) {
+                saveSwarm = FALSE, runAsShiny = FALSE) {
   stopifnot(length(lb) == length(ub))
+  if(length(lb) != length(ub)) return(ErrorClass$new("length of lb and ub differ"))
+  if(!is.numeric(lb)) return(ErrorClass$new("lb has to be of type numeric"))
+  if(!is.numeric(ub)) return(ErrorClass$new("ub has to be of type numeric"))
+  if(!is.function(loss)) return(ErrorClass$new("loss has to be of type function"))
+  if(!is.numeric(ngen)) return(ErrorClass$new("ngen has to be of type numeric"))
+  if(!is.numeric(npop)) return(ErrorClass$new("npop has to be of type numeric"))
+  if(length(ngen) != 1) return(ErrorClass$new("ngen has to be a scalar"))
+  if(length(npop) != 1) return(ErrorClass$new("npop has to be a scalar"))
+  if(length(lb) <= 0) return(ErrorClass$new("lb and ub need at least one element"))
+  if(ngen <= 10) return(ErrorClass$new("ngen has to be at least 10"))  
+  if(npop <= 5) return(ErrorClass$new("npop has to be at least 5"))
+  if(!is.numeric(error_threshold)) return(ErrorClass$new("error_threshold has to be of type numeric"))
+  if(length(error_threshold) != 1) return(ErrorClass$new("error_threshold has to be a scalar"))
+  if(!is.logical(global)) return(ErrorClass$new("global has to be of type logical"))
+  if(length(global) != 1) return(ErrorClass$new("global has to be a scalar"))
+  if(!is.logical(saveSwarm)) return(ErrorClass$new("saveSwarm has to be of type logical"))
+  if(length(saveSwarm) != 1) return(ErrorClass$new("saveSwarm has to be a scalar"))
+  if(any(lb > ub)) {
+    w <- which(lb > ub)
+    w <- paste(w, collapse = ", ")
+    return(ErrorClass$new(paste("lb > ub for: ", w)))
+  }
+
   npar = length(lb)
   swarm <- matrix(0, nrow = npop, ncol = npar)
   v <- matrix(0, nrow = npop, ncol = npar)
@@ -125,15 +189,16 @@ pso <- function(env, lb, ub, loss, ngen, npop, error_threshold, global = FALSE,
           }
           gbv <- c(kX = global_best_vec[1], I0 = global_best_vec[2],
                    IHD = global_best_vec[3], ID = global_best_vec[4])
+          gbv <- format(round(gbv, digits = 6), nsmall = 6)
           gbv <- Map(function(a, b) {
             paste(a, " = ", b)
           }, names(gbv), gbv) |> unlist() |> paste(collapse = ", ")
-          runAsShiny$running((100/ngen) * iter)
+          runAsShiny$running( format(round( (100.0/ngen) * iter, digits = 6), nsmall = 6) )
           runAsShiny$setData(
             paste(
-              paste0((100/ngen) * iter, "% completed;"),
+              paste0( format(round( (100.0/ngen) * iter, digits = 6), nsmall = 6), "% completed;"),
               gbv,
-              "; Error: ", global_best_error,
+              "; Error: ", format(round(global_best_error, digits = 6), nsmall = 6),
               collapse = "\n"
             )
           )
