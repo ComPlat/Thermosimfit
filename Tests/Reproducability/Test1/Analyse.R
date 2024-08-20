ec9ec104cd3c20ee5543f7ba6b1070bf4edd3fd1
Konrad1991 <konrad_kraemer@yahoo.de>
2024-08-20 15:07:33 +0200

    combined the measurement in Reproducability/Test1 in the file Combined. This can now be used to test batch procesing

--- Tests/Reproducability/Test1/Analyse.R
+++ Tests/Reproducability/Test1/Analyse.R
@@ -1,9 +1,19 @@
 library(tsf)
 library(ggplot2)
 library(patchwork)
-setwd("/home/konrad/Documents/GitHub/Thermosimfit/Reproducability/Test1")
+setwd("/home/konrad/Documents/GitHub/RProjects/Thermosimfit/Tests/Reproducability/Test1")
 files <- list.files(pattern = "*.txt")
-kHD <- 2398.833 * 10^6 # log Ka = 3.38 
+
+l <- lapply(files, function(x) {
+  d <- read.table(x, header = FALSE)
+  d <- apply(d, 2, as.character)
+  d <- rbind(c("Var", "Signal"), d)
+  return(d)
+})
+l <- Reduce(rbind, l)
+write.csv(l, file = "Combined", row.names = FALSE, quote = FALSE)
+
+kHD <- 2398.833 * 10^6 # log Ka = 3.38
 H0 <- 100 # µM
 d0 <- 100 # µM
 # guest from 0 - 366 µM
@@ -14,18 +24,22 @@ ID <- 2.48e06 # 2.48 when using µM
 # kGuest = 6.45e4, 1.14e5, 1.05e5, 7.3e4, 1.19e5, 5.63e4, 3.4e4, 4.87e4
 
 # grenzen enger setzten
-res <- opti("ida", c(1, 0, 0, 0), c(10^10, 10*10^-4, 100, 100), files[[1]], c(H0, d0, kHD),
-            npop = 40, ngen = 100, Topology = "random") # 400
+res <- opti("ida", c(1, 0, 0, 0), c(10^10, 10 * 10^-4, 100, 100), files[[1]], c(H0, d0, kHD),
+  npop = 40, ngen = 100, Topology = "random"
+) # 400
 res
 res[[2]]
 c <- tsf:::Communicator$new()
 tsf::sensitivity("ida", res[[2]], files[[1]], c(H0, d0, kHD),
-                 15, runAsShiny = c)
+  15,
+  runAsShiny = c
+)
 
 res <- lapply(files, function(x) {
   l <- lapply(1:5, function(y) {
-    opti("ida", c(1, 0, 0, 0), c(10^10, 10*10^-4, 100, 100), files[[1]], c(H0, d0, kHD),
-         npop = 40, ngen = 400, Topology = "random")
+    opti("ida", c(1, 0, 0, 0), c(10^10, 10 * 10^-4, 100, 100), files[[1]], c(H0, d0, kHD),
+      npop = 40, ngen = 400, Topology = "random"
+    )
   })
   names(l) <- paste0(1:5, x)
   return(l)
@@ -44,11 +58,11 @@ d <- lapply(res, function(x) {
 d <- Reduce(rbind, d)
 names(d)[6] <- "file"
 d <- tidyr::pivot_longer(d, cols = -c(guest, file))
-d$group <- ifelse(d$name != "d", "signal", "d")  
+d$group <- ifelse(d$name != "d", "signal", "d")
 d$group <- ifelse(d$name == "hd", "hd", d$group)
 ggplot(data = d, aes(x = guest, y = value, colour = factor(file))) +
   geom_point() +
-  facet_wrap(.~ group, scales = "free")
+  facet_wrap(. ~ group, scales = "free")
 
 params <- lapply(res, function(x) {
   l <- lapply(x, function(y) {
@@ -62,30 +76,26 @@ means <- lapply(params, function(x) {
   apply(x, 2, mean)
 })
 means <- Reduce(rbind, means)
-means[,1] <- means[, 1] / 10^6
+means[, 1] <- means[, 1] / 10^6
 means[, 2:4] <- means[, 2:4] * 10^6
 
 format(means, scientific = TRUE)
 # kguest         I0             IHD            ID             parent.frame()$i[]
-# init "2.153240e+04" "2.551025e+02" "1.520668e+07" "9.525392e+06" "1.000000e+00"    
-# "2.799020e+04" "6.884824e+02" "1.534880e+07" "9.592180e+06" "2.000000e+00"    
-# "4.544291e+04" "6.210149e+02" "1.513578e+07" "9.914334e+06" "3.000000e+00"    
-# "1.044127e+04" "1.508630e+02" "1.507873e+07" "8.488497e+06" "4.000000e+00"    
-# "2.490850e+04" "6.666667e+02" "1.495009e+07" "9.367511e+06" "5.000000e+00"    
-# "4.260270e+03" "3.264909e+02" "1.497401e+07" "7.709176e+06" "6.000000e+00"    
-# "7.210665e+03" "1.842407e+02" "1.519122e+07" "7.855857e+06" "7.000000e+00"    
+# init "2.153240e+04" "2.551025e+02" "1.520668e+07" "9.525392e+06" "1.000000e+00"
+# "2.799020e+04" "6.884824e+02" "1.534880e+07" "9.592180e+06" "2.000000e+00"
+# "4.544291e+04" "6.210149e+02" "1.513578e+07" "9.914334e+06" "3.000000e+00"
+# "1.044127e+04" "1.508630e+02" "1.507873e+07" "8.488497e+06" "4.000000e+00"
+# "2.490850e+04" "6.666667e+02" "1.495009e+07" "9.367511e+06" "5.000000e+00"
+# "4.260270e+03" "3.264909e+02" "1.497401e+07" "7.709176e+06" "6.000000e+00"
+# "7.210665e+03" "1.842407e+02" "1.519122e+07" "7.855857e+06" "7.000000e+00"
 
 params <- Reduce(rbind, params)
 names(params) <- c("kguest", "I0", "IHD", "ID", "file")
 params <- tidyr::pivot_longer(params, cols = -file)
-params$value <- params$value / 10^6 
+params$value <- params$value / 10^6
 
 ggplot(data = params, aes(x = file, y = value, colour = file)) +
   geom_boxplot(data = params, aes(x = file, y = value, group = file, colour = file)) +
   geom_point() +
-  facet_wrap(.~ name, scales = "free") +
+  facet_wrap(. ~ name, scales = "free") +
   stat_summary(aes(group = name), fun.y = mean, geom = "line")
-
-
-
-
