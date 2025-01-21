f717d7ce89acd4860d0733eec3de9c582895eace
Konrad1991 <konrad_kraemer@yahoo.de>
2025-01-21 12:49:51 +0100

    update sensitivity plot according to: https://github.com/ComPlat/Thermosimfit/issues/42

--- Paper/Sensitivity/FigNr2.R
+++ Paper/Sensitivity/FigNr2.R
@@ -2,6 +2,17 @@ library(ggplot2)
 library(cowplot)
 load("Sensitivity.RData")
 load("ErrorsVsImportantParams.RData")
+dotsize <- 0.5
+boxplot_size <- 0.5
+outlier_size <- 0.5
+axis <- element_text(size = 8)
+axis_title <- element_text(size = 12)
+legend_text <- element_text(size = 8)
+theme <- theme(
+  axis.text = axis,
+  axis.title = axis_title,
+  legend.text = legend_text
+)
 
 # Add correct labels to sensitivity plots
 add_labels <- function(pl, names) {
@@ -33,63 +44,33 @@ p_gda <- add_labels(p_gda, c(
   "I(0)*I(HD)", "I(0)*I(D)", "I(HD)*I(D)"
 ))
 
+p_ida <- p_ida + theme
+p_dba <- p_dba + theme
+p_gda <- p_gda + theme
+
+ggsave(p_ida,
+  bg = "white",
+  width = 5,
+  height = 6,
+  dpi = 900,
+  file = "FigNr2.png"
+)
+
 empty_plot <- ggplot() +
   theme_void()
 size_dashes <- 0.25
 
 sensi_plots <- plot_grid(
-  p_dba, empty_plot, p_ida, empty_plot, p_gda,
-  labels = c("a", "", "b", "", "c"),
-  ncol = 5,
-  rel_widths = c(1, 0.05, 1, 0.05, 1)
-)
-sensi_plots <- ggdraw(sensi_plots) +
-  draw_line(
-    x = c(0.33, 0.33), y = c(0, 1),
-    color = "black",
-    linetype = "dashed",
-    size = size_dashes
-  ) +
-  draw_line(
-    x = c(0.66, 0.66),
-    y = c(0, 1),
-    color = "black",
-    linetype = "dashed",
-    size = size_dashes
-  )
-
-ip_plots <- plot_grid(
-  p_dba_ip, empty_plot, p_ida_ip, empty_plot, p_gda_ip,
-  labels = c("d", "", "e", "", "f"),
-  ncol = 5,
-  rel_widths = c(1, 0.05, 1, 0.05, 1)
+  p_dba, empty_plot, p_gda,
+  labels = c("a", "", "b"),
+  ncol = 3,
+  rel_widths = c(1, 0.05, 1)
 )
 
-ip_plots <- ggdraw(ip_plots) +
-  draw_line(
-    x = c(0.33, 0.33), y = c(0, 1),
-    color = "black",
-    linetype = "dashed",
-    size = size_dashes
-  ) +
-  draw_line(
-    x = c(0.66, 0.66),
-    y = c(0, 1),
-    color = "black",
-    linetype = "dashed",
-    size = size_dashes
-  )
-
-p <- plot_grid(
-  sensi_plots,
-  ip_plots,
-  nrow = 2
-)
-
-ggsave(p,
+ggsave(sensi_plots,
   bg = "white",
-  width = 20,
-  height = 10,
+  width = 10,
+  height = 6,
   dpi = 900,
-  file = "FigNr2.png"
+  file = "SuplFigNr1.png"
 )
